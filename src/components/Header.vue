4ac9ff831508c0e7dcc9b359c39ef902b5768a0e
linhui45 <frame_lin@163.com>
2019-01-21 07:57:20 +0800

    页头修改

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,12 +1,41 @@
 <template>
-  <div>
-    <el-input value="name's"/>
+  <div class="header-box">
+    <el-header class="news_tnav_box map_tnav_box">
+      <dt><a href="javascript:;">搜艇网</a>，
+        <strong><a href="javascript:;" title="三亚游艇租赁">三亚游艇租赁</a></strong>,
+        <strong><a href="javascript:;" title="三亚游艇出租">三亚游艇出租</a></strong>,
+        <strong><a href="javascript:;" title="游艇销售">游艇销售</a></strong>,
+        <strong><a href="javascript:;" title="三亚游艇">三亚游艇</a></strong>,
+        <strong><a href="javascript:;" title="三亚包船">三亚包船</a></strong>,全国首家游艇O2O一站式服务平台！
+      </dt>
+      <dd class="top_zhuce">
+        <ul>
+          <li><img src="/static/icon/icon03.jpg"/><a href="javascript:;" id="zhuce">用户注册</a></li>
+          <li><span>|</span><img src="/static/icon/icon04.jpg"/><a href="javascript:;" id="denglu">会员登录</a></li>
+          <li><span>|</span><img src="/static/icon/icon06.jpg"/>咨询热线：<b>18876987699</b></li>
+        </ul>
+      </dd>
+    </el-header>
   </div>
 </template>
 <script>
   export default {
     name: 'Header',
+    methods:{
+      on:function () {
+        window.open("localhost:8089/yacht/hello");
+      }
+    }
   }
 </script>
-<style lang="less">
+<style lang="scss" rel="stylesheet/scss" scoped>
+  @import "../common/style/theme";
+
+  .header-box {
+    background: $head-bgc;
+    background-image: -webkit-linear-gradient(#000, #121212);
+    background-image: linear-gradient(#000, #121212);
+    width: 100%;
+
+  }
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from '../views/Home' // 导入layouts.vue 组件
+import HelloWorld from '../components/HelloWorld' // 导入layouts.vue 组件
 
 Vue.use(Router);
 
@@ -10,6 +11,10 @@ export default new Router({
       path: '/',  //访问路径
       name: 'Home', // 路径名
       component: Home //访问的组件,即访问‘/’,它会加载 Layouts 组件所有的内容。
+    },{
+      path: '/',  //访问路径
+      name: 'HelloWorld', // 路径名
+      component: HelloWorld //访问的组件,即访问‘/’,它会加载 Layouts 组件所有的内容。
     }
   ]
 })
